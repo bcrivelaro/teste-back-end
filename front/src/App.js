30bcdf776b69abf1d84aea7c7d0a10f323640bb0
Bruno Crivelaro <brunosantoscrivelaro@gmail.com>
2020-10-22 00:09:06 -0300

    feat: build main App component

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ContactState from './context/contacts/ContactState';
+import VisitState from './context/visits/VisitState';
+import Contacts from './components/contacts/Contacts';
+import Navbar from './components/layout/Navbar';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
+import Visits from './components/visits/Visits';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ContactState>
+      <VisitState>
+        <BrowserRouter>
+          <div className='App'>
+            <Navbar />
+            <div className='container'>
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/visits' component={Visits} />
+                <Route exact path='/contacts' component={Contacts} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </BrowserRouter>
+      </VisitState>
+    </ContactState>
   );
 }
 
